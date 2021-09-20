4f944dcb94f92bce83176476b7216fb161e9830e
czczup <wztxy89@163.com>
2021-09-20 21:01:33 +0800

    fix a bug

--- compile.sh
+++ compile.sh
@@ -2,6 +2,4 @@ cd ./models/post_processing/pa/
 python setup.py build_ext --inplace
 cd ../pse/
 python setup.py build_ext --inplace
-cd ../pse_v2/
-python setup.py build_ext --inplace
 cd ../../../
