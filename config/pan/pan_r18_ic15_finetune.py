3eafd84cf88389924007e76eb398e91045ec9963
czczup <wztxy89@163.com>
2021-09-21 13:40:03 +0800

    update

--- config/pan/pan_r18_ic15_finetune.py
+++ config/pan/pan_r18_ic15_finetune.py
@@ -1,37 +1,62 @@
-model = dict(type='PAN',
-             backbone=dict(type='resnet18', pretrained=True),
-             neck=dict(type='FPEM_v1',
-                       in_channels=(64, 128, 256, 512),
-                       out_channels=128),
-             detection_head=dict(type='PA_Head',
-                                 in_channels=512,
-                                 hidden_dim=128,
-                                 num_classes=6,
-                                 loss_text=dict(type='DiceLoss',
-                                                loss_weight=1.0),
-                                 loss_kernel=dict(type='DiceLoss',
-                                                  loss_weight=0.5),
-                                 loss_emb=dict(type='EmbLoss_v1',
-                                               feature_dim=4,
-                                               loss_weight=0.25)))
-data = dict(batch_size=16,
-            train=dict(type='PAN_IC15',
-                       split='train',
-                       is_transform=True,
-                       img_size=736,
-                       short_size=736,
-                       kernel_scale=0.5,
-                       read_type='cv2'),
-            test=dict(type='PAN_IC15',
-                      split='test',
-                      short_size=736,
-                      read_type='cv2'))
-train_cfg = dict(lr=1e-3,
-                 schedule='polylr',
-                 epoch=600,
-                 optimizer='Adam',
-                 pretrain='checkpoints/pan_r18_synth/checkpoint_1ep.pth.tar')
-test_cfg = dict(min_score=0.85,
-                min_area=16,
-                bbox_type='rect',
-                result_path='outputs/submit_ic15.zip')
+model = dict(
+    type='PAN',
+    backbone=dict(
+        type='resnet18',
+        pretrained=True
+    ),
+    neck=dict(
+        type='FPEM_v1',
+        in_channels=(64, 128, 256, 512),
+        out_channels=128
+    ),
+    detection_head=dict(
+        type='PA_Head',
+        in_channels=512,
+        hidden_dim=128,
+        num_classes=6,
+        loss_text=dict(
+            type='DiceLoss',
+            loss_weight=1.0
+        ),
+        loss_kernel=dict(
+            type='DiceLoss',
+            loss_weight=0.5
+        ),
+        loss_emb=dict(
+            type='EmbLoss_v1',
+            feature_dim=4,
+            loss_weight=0.25
+        )
+    )
+)
+data = dict(
+    batch_size=16,
+    train=dict(
+        type='PAN_IC15',
+        split='train',
+        is_transform=True,
+        img_size=736,
+        short_size=736,
+        kernel_scale=0.5,
+        read_type='cv2'
+    ),
+    test=dict(
+        type='PAN_IC15',
+        split='test',
+        short_size=736,
+        read_type='cv2'
+    )
+)
+train_cfg = dict(
+    lr=1e-3,
+    schedule='polylr',
+    epoch=600,
+    optimizer='Adam',
+    pretrain='checkpoints/pan_r18_synth/checkpoint_1ep.pth.tar'
+)
+test_cfg = dict(
+    min_score=0.85,
+    min_area=16,
+    bbox_type='rect',
+    result_path='outputs/submit_ic15.zip'
+)
