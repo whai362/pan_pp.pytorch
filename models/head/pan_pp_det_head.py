0a5bc9b81eebbe94448f2ecb66b0c63e88e36f18
zerohertz <ohg3417@gmail.com>
2023-03-09 20:43:56 +0900

    :bug: Fix: cv2.resize

--- models/head/pan_pp_det_head.py
+++ models/head/pan_pp_det_head.py
@@ -93,9 +93,9 @@ class PAN_PP_DetHead(nn.Module):
         label_num = np.max(label) + 1
         scale = (float(org_img_size[1]) / float(img_size[1]),
                  float(org_img_size[0]) / float(img_size[0]))
-        label = cv2.resize(label, (img_size[1], img_size[0]),
+        label = cv2.resize(label, (int(img_size[1]), int(img_size[0])),
                            interpolation=cv2.INTER_NEAREST)
-        score = cv2.resize(score, (img_size[1], img_size[0]),
+        score = cv2.resize(score, (int(img_size[1]), int(img_size[0])),
                            interpolation=cv2.INTER_NEAREST)
 
         with_rec = hasattr(cfg.model, 'recognition_head')
